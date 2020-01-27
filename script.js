99f517aa3014b84623468aa9254707520bd9b998
Pttstr <example.gmail.com>
2020-01-27 23:17:43 +0300

    empty quotation marks in 'prompt' added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
@@ -10,10 +10,10 @@ let appData = {
     savings: false,
 }
 
-let firstQ1 = prompt("Введите обязательную статью расходов в этом месяце"); // first point of expenses
-let secondQ1 = prompt("Во сколько обойдется?"); // value of first point of expenses
-let firstQ2 = prompt("Введите обязательную статью расходов в этом месяце"); // second point of expenses
-let secondQ2 = prompt("Во сколько обойдется?"); //value of second point of expenses
+let firstQ1 = prompt("Введите обязательную статью расходов в этом месяце", ""); // first point of expenses
+let secondQ1 = prompt("Во сколько обойдется?", ""); // value of first point of expenses
+let firstQ2 = prompt("Введите обязательную статью расходов в этом месяце", ""); // second point of expenses
+let secondQ2 = prompt("Во сколько обойдется?", ""); //value of second point of expenses
 
 appData.expenses[firstQ1] = secondQ1;
 appData.expenses[firstQ2] = secondQ2;
